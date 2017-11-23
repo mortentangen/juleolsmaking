aad84258accd990f6a07d2fb7d92c82e09571ea7
Morten S. Tangen <morten.tangen@gmail.com>
2017-11-23 21:49:05 +0100

    add transition from client to result

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Client = () =>
-	<div>Logged in</div>;
+	<div>
+		<div>Logged in</div>
+		<div><Link to="/">Board</Link></div>
+	</div>;
 
 export default Client;
