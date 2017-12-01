bc9c8a5e308df32b8ef019d97c8903bf2ca02145
Morten S. Tangen <morten.tangen@gmail.com>
2017-12-01 22:54:41 +0100

    initial styling of Board + snow :)

--- src/Board/BeerItem/BeerItem.js
+++ src/Board/BeerItem/BeerItem.js
@@ -8,29 +8,32 @@ import { getUserScore, getTotalBeerScore } from '../../vote-service';
 
 const BeerItem = ({ beer, votes }) =>
 	<div key={beer.id} className="BeerItem_container">
-		<div className="BeerItem_beerImage">
-			<BeerImage image={beer.image} />
-		</div>
-		<div className="BeerItem_row">
-			<div className="BeerItem_description">
-				<Link to={`/userboard/${beer.id}`}>
-					<div className="BeerItem_beerName">
-						<div>{beer.brand}</div>
-						<div> {beer.name} ({beer.year})</div>
-					</div>
-				</Link>
+		<div className="BeerItem_innerContainer">
+			<div className="BeerItem_beerImage">
+				<BeerImage image={beer.image} />
 			</div>
-			<div className="BeerItem_individualScore">
-				{
-					Object.entries(votes).map(([key, value]) =>
-						<div key={key}><span>{key.substring(0, 3)}</span><Stars nr={getUserScore(value)} size={14} /></div>
-					)
-				}
+			<div className="BeerItem_row">
+				<div className="BeerItem_description">
+					<Link to={`/userboard/${beer.id}`}>
+						<div className="BeerItem_beerName">
+							<div>{beer.brand}</div>
+							<div> {beer.name} ({beer.year})</div>
+						</div>
+					</Link>
+				</div>
+				<div className="BeerItem_individualScore">
+					{
+						Object.entries(votes).map(([key, value]) =>
+							<div key={key}><span>{key.substring(0, 3)}</span><Stars nr={getUserScore(value)}
+							                                                        size={14} /></div>
+						)
+					}
+				</div>
 			</div>
-		</div>
-		<div className="BeerItem_score">
-			<span>{getTotalBeerScore(votes)}</span>
-			<span className="BeerItem_star"><Stars nr={1} /></span>
+			<div className="BeerItem_score">
+				<span>{getTotalBeerScore(votes)}</span>
+				<span className="BeerItem_star"><Stars nr={1} /></span>
+			</div>
 		</div>
 	</div>;
 
--- src/Board/Board.js
+++ src/Board/Board.js
@@ -3,22 +3,21 @@ import { Link } from 'react-router-dom'
 import BeerItem from './BeerItem/BeerItem';
 import connect from '../connect';
 import { sortByScore } from '../vote-service';
+import Snow from 'react-snow-effect';
 import './Board.css';
 
 const Board = ({ beer, votes }) => {
 	const sorteredBeer = sortByScore(votes);
 	return (
 		<div>
-
-		<div>
-			{
-				sorteredBeer.map(({ beerId }) =>
-					<BeerItem key={beerId} beer={beer[beerId]} votes={votes[beerId] || {}} />
-				)
-			}
-		</div>
-			<div className="Board_gotoUserBoard">
-				<Link to="/userboard">Delta</Link>
+			<div className="Board_snow"><Snow /></div>
+			<div>Brukere kommer her</div>
+			<div>
+				{
+					sorteredBeer.map(({ beerId }) =>
+						<BeerItem key={beerId} beer={beer[beerId]} votes={votes[beerId] || {}} />
+					)
+				}
 			</div>
 		</div>
 	)
