72f47c524d4c2b7a38f7016deca82454462e42db
Morten S. Tangen <morten.tangen@gmail.com>
2017-12-08 20:18:04 +0100

    fix sorting

--- src/vote-service.js
+++ src/vote-service.js
@@ -1,11 +1,14 @@
 const getUserScore = votes => Object.values(votes).reduce((a, b) => a + b, 0);
 const getTotalBeerScore = allBeerVotes => Object.values(allBeerVotes).reduce((a, b) => a + getUserScore(b), 0);
-const sortByScore = votes =>
-	Object.entries(votes)
-		.map(([beerId, beerVotes]) => ({
+const sortByScore = votes => Object.entries(votes)
+	.map(([beerId, beerVotes]) => {
+		return {
 			beerId,
 			score: getTotalBeerScore(beerVotes)
-		}))
-		.sort(((a, b) => a.score < b.score ));
+		}
+	})
+	.sort(((a, b) => {
+		return b.score - a.score
+	} ));
 
 export { getUserScore, getTotalBeerScore, sortByScore };
