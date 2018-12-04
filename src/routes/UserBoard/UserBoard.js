46d85eae1a88c6966422af1cdb54132abe88053e
Morten S. Tangen <morten.tangen@gmail.com>
2018-12-04 19:28:17 +0100

    react 16.6.3

--- src/routes/UserBoard/UserBoard.js
+++ src/routes/UserBoard/UserBoard.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import User from '../../modules/User/User';
-import UserBeerItem from './UserBeerItem/UserBeerItem';
-import './UserBoard.css'
-import connect from '../../connect';
+import React from "react";
+import User from "../../modules/User/User";
+import UserBeerItem from "./UserBeerItem/UserBeerItem";
+import "./UserBoard.css";
+import connect from "../../connect";
 
 const UserBoard = ({ beer, history }) => {
-	const beerList = Object.entries(beer).map(keyValue => ({ id: keyValue[0], ...keyValue[1] }));
-	const beerList2018 = beerList.filter(beer => beer.yearTasted === '2018');
-	return (
-		<div>
-			<div className="UserBoard_user">
-				<User history={history} />
-			</div>
-			<div className="UserBoard_list">
-				{
-					beerList2018.map(beer =>
-						<UserBeerItem key={beer.id} beer={beer} />
-					)
-				}
-			</div>
-		</div>
-	);
+  const beerList = Object.entries(beer).map(keyValue => ({
+    id: keyValue[0],
+    ...keyValue[1]
+  }));
+  const beerList2018 = beerList.filter(beer => beer.yearTasted === "2018");
+  return (
+    <div>
+      <div className="UserBoard_user">
+        <User history={history} />
+      </div>
+      <div className="UserBoard_list">
+        {beerList2018.map(beer => (
+          <UserBeerItem key={beer.id} beer={beer} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 const setStateFromSnapshot = snapshot => (prevState, props) => ({
-	beer: snapshot.val()
+  beer: snapshot.val()
 });
 
-export default connect('beer', () => 'beer', setStateFromSnapshot)(UserBoard);
+export default connect(
+  "beer",
+  () => "beer",
+  setStateFromSnapshot
+)(UserBoard);
