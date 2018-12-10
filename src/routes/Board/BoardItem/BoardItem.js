b95a2a8abc4810338c46996d66f045532dc373e6
Morten S. Tangen <morten.tangen@gmail.com>
2018-12-10 19:02:50 +0100

    remove Link from Board

--- src/routes/Board/BoardItem/BoardItem.js
+++ src/routes/Board/BoardItem/BoardItem.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import BeerImage from '../../../modules/BeerImage/BeerImage';
-// TODO move stars
 import Stars from '../../../modules/Stars/Stars';
 import './BoardItem.css';
 import { getUserScore, getTotalBeerScore } from '../../../vote-service';
@@ -14,12 +12,10 @@ const BoardItem = ({ beer, votes, usersWithColors, currentYear }) => (
       </div>
       <div className="BoardItem_row">
         <div className="BoardItem_description">
-          <Link to={`/userboard/${currentYear}/${beer.id}`}>
-            <div className="BoardItem_beerName">
-              <div>{beer.brand}</div>
-              <div> {beer.name}</div>
-            </div>
-          </Link>
+          <div className="BoardItem_beerName">
+            <div>{beer.brand}</div>
+            <div> {beer.name}</div>
+          </div>
         </div>
         <div className="BoardItem_individualScore">
           {Object.entries(votes).map(([userId, value]) => (
